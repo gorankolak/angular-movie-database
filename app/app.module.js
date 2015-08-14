560be2cd4d0f4ce2473823409d64d1628c3b888e
gorankolak <goran.kolak82@gmail.com>
2015-08-14 17:14:57 +0200

    Typo fix

--- app/app.module.js
+++ app/app.module.js
@@ -2,8 +2,8 @@
     'use strict';
     
     angular.module('app', [
-        'ngRoute', 
-        'ngStorage', 
+        'ngStorage',
+        'ngRoute',  
         'chart.js', 
         'angularUtils.directives.dirPagination'
     ]);
